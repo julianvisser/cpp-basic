e581e76a1ee2316f89253a61df91d70c3ec584c6
julianvisser <61542390+julianvisser@users.noreply.github.com>
2020-10-01 00:28:31 +0200

    Add files via upload

---
+++ Opdracht 1: Lesrooster/Docent.cpp
@@ -0,0 +1,14 @@
+// Class voor docenten. Hieronder wat lege variabelen en methodes die hierbij zouden horen. 
+
+
+string name;
+
+float yearlySalary;
+
+string role; // Studieleider, kerndocent, directeur, etc.
+
+// Met een methode zoals dit kan men de variabelen opvragen
+
+string getName(){
+    return name; 
+}
